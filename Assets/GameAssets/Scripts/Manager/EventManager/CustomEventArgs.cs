036c032046a99f5a009bfa58a8450dad6a1bfd00
Dulst <dulstaminos@163.com>
2026-05-28 21:31:02 +0800

    feat: 实现基础事件系统

---
+++ Assets/GameAssets/Scripts/Manager/EventManager/CustomEventArgs.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+// ======== 储存事件参数类 ========
+
+public class TestEventArgs : EventArgs
+{
+    public string testString;
+}
---
+++ Assets/GameAssets/Scripts/Manager/EventManager/EventName.cs
@@ -0,0 +1,7 @@
+﻿/// <summary>
+/// 储存事件名称
+/// </summary>
+public static class EventName
+{
+    public const string Test = nameof(Test);
+}
